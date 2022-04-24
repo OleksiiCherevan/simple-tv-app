036e4362531ff977233b10a44343bd7366186c82
Alex <lolmagexe@gmail.com>
2022-04-24 12:32:42 +0300

    (feature) added test data desktop/tablet/mobile

---
+++ src/js/mochData.js
@@ -0,0 +1,24 @@
+import crown from "./../assets/images/crown.jpg"
+
+
+export const shows = {
+    shows: [
+        {
+            text: 'The crown',
+            img: crown,
+
+        }, 
+        {
+            text: 'Oliver',
+            img: crown,
+
+        }, 
+        
+        {
+            text: 'Gray',
+            img: crown,
+
+        }, 
+        
+    ]
+}
